09cd685d8de166e4895bd92f4f4349dca7674306
FFFK10 <fffk10@MacBook-Air.local>
2021-05-27 20:37:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/acceleget/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.acceleget
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    /**
+     * センサーの精度が変更されると呼び出される
+     *　Not implemented
+     */
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    /**
+     * センサーの値が変更されるたびに呼び出される
+     */
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) return
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            txt01.text = "X軸：${event.values[0]}\n" +
+                    "Y軸：${event.values[1]}\n" +
+                    "Z軸：${event.values[2]}"
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Objectの戻り値をSensorManager型にcast
+        // センサーマネージャーのインスタンスを取得
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        // センサーのタイプを指定してSensorオブジェクト(accelerometer)を取得
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.unregisterListener(this)
+    }
+}
